950ad55189b0f93492b33a3dbbb45a3b9bcae1e3
Bálint Áts <atsbalint@gmail.com>
2019-10-28 16:34:46 +0100

    Using datacontext instead of property.

---
+++ SanityArchiver/SanityArchiver.DesktopUI/ViewModels/CompressedFileVm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace SanityArchiver.Application.Models
+{
+    /// <summary>
+    /// Model for containing the files to compress
+    /// </summary>
+    public class CompressedFileVm
+    {
+        private ObservableCollection<FileInfo> _filesToCompress = new ObservableCollection<FileInfo>();
+        private string _compressedName;
+
+        /// <summary>
+        /// Gets or sets the observableCollection of files to compress
+        /// </summary>
+        public ObservableCollection<FileInfo> FilesToCompress
+        {
+            get
+            {
+                return _filesToCompress;
+            }
+
+            set
+            {
+                if (_filesToCompress != value)
+                {
+                    _filesToCompress = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets
+        /// </summary>
+        public string CompressedName
+        {
+            get
+            {
+                return _compressedName;
+            }
+
+            set
+            {
+                if (_compressedName != value)
+                {
+                    _compressedName = value;
+                }
+            }
+        }
+    }
+}
--- SanityArchiver/SanityArchiver.DesktopUI/Views/CompressMenu.xaml.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/CompressMenu.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +22,29 @@ namespace SanityArchiver.DesktopUI.Views
     /// </summary>
     public partial class CompressMenu : Window
     {
-        private CompressedFile _compressedFile = new CompressedFile();
+        private readonly CompressedFileVm _compressedFile;
+
+        //private CompressedFile _compressedFile = new CompressedFile();
+
+        /// <summary>
+        /// The
+        /// </summary>
         public CompressMenu()
         {
             InitializeComponent();
+            _compressedFile = new CompressedFileVm();
+            this.DataContext = _compressedFile;
         }
-        public CompressedFile CompresssedFile
+
+        /// <summary>
+        /// Gets or Sets
+        /// </summary>
+       /* public CompressedFile CompresssedFile
         {
-            get { return _compressedFile; }
+            get
+            {
+                return _compressedFile;
+            }
 
             set
             {
@@ -36,18 +53,47 @@ namespace SanityArchiver.DesktopUI.Views
                     _compressedFile = value;
                 }
             }
+        }*/
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            string zipFileName = string.Format("zipfile-{0:yyyy-MM-dd_hh-mm-ss-tt}.zip", DateTime.Now);
+            string dir = @"C:\Users\Áts Bálint\codecool\test_dir\archiveMeBroTestFiles";
+
+            //_compressedFile.FilesToCompress.First().DirectoryName;
+
+            using (MemoryStream zipMS = new MemoryStream())
+            {
+                using (ZipArchive zipArchive = new ZipArchive(zipMS, ZipArchiveMode.Create, true))
+                {
+                    foreach (FileInfo fileToZip in _compressedFile.FilesToCompress)
+                    {
+                        //read the file bytes
+                        byte[] fileToZipBytes = System.IO.File.ReadAllBytes(fileToZip.FullName);
+
+                        //create the entry - this is the zipped filename
+                        //change slashes - now it's VALID
+                        ZipArchiveEntry zipFileEntry = zipArchive.CreateEntry(fileToZip.FullName.Replace(dir, string.Empty).Replace('\\', '/'));
+
+                        //add the file contents
+                        using (Stream zipEntryStream = zipFileEntry.Open())
+                        using (BinaryWriter zipFileBinary = new BinaryWriter(zipEntryStream))
+                        {
+                            zipFileBinary.Write(fileToZipBytes);
+                        }
+                    }
+                }
+
+                using (FileStream finalZipFileStream = new FileStream(dir + zipFileName, FileMode.Create))
+                {
+                    zipMS.Seek(0, SeekOrigin.Begin);
+                    zipMS.CopyTo(finalZipFileStream);
+                }
+            }
         }
 
-
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Cancel_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
