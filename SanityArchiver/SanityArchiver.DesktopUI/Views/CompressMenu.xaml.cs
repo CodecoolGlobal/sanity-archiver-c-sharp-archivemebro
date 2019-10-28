d21d4d533c269e967b8acd332191574bd94155c7
Bálint Áts <atsbalint@gmail.com>
2019-10-28 11:51:36 +0100

    Created model, and view for CompressedFile.

---
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/CompressMenu.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SanityArchiver.Application.Models;
+
+namespace SanityArchiver.DesktopUI.Views
+{
+    /// <summary>
+    /// Interaction logic for CompressMenu.xaml
+    /// </summary>
+    public partial class CompressMenu : Window
+    {
+        private CompressedFile _compressedFile = new CompressedFile();
+        public CompressMenu()
+        {
+            InitializeComponent();
+        }
+        public CompressedFile CompresssedFile
+        {
+            get { return _compressedFile; }
+
+            set
+            {
+                if (_compressedFile != value)
+                {
+                    _compressedFile = value;
+                }
+            }
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
